730e4cee3c2a561fc0c9ba8ede87aeb94a0242aa
yangk <yangk@xuhaoyudeMacBook-Pro.local>
2020-11-13 15:32:06 +0800

    modified sh

--- release.sh
+++ release.sh
@@ -10,20 +10,20 @@ fi
 BUILD_RBD_APP_UI=${BUILD_RBD_APP_UI:-true} 
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
-docker build -t "rainbond/rainbond-ui:$VERSION" .
-docker push "rainbond/rainbond-ui:$VERSION"
+docker build -t "yangk/rainbond-ui:$VERSION" .
+docker push "yangk/rainbond-ui:$VERSION"
 
 
 if [ ${BUILD_RBD_APP_UI} == "true" ];
 then
 	mv dist build/dist
-	docker build --build-arg VERSION="${VERSION}" -t "rainbond/rbd-app-ui:$VERSION" ./build
-	docker push "rainbond/rbd-app-ui:$VERSION"
+	docker build --build-arg VERSION="${VERSION}" -t "yangk/rbd-app-ui:$VERSION" ./build
+	docker push "yangk/rbd-app-ui:$VERSION"
 
 	if [ ${DOMESTIC_BASE_NAME} ];
 	then
 		newTag="${DOMESTIC_BASE_NAME}/${DOMESTIC_NAMESPACE}/rbd-app-ui:${VERSION}"
-		docker tag "rainbond/rbd-app-ui:$VERSION" "${newTag}"
+		docker tag "yangk/rbd-app-ui:$VERSION" "${newTag}"
 		docker login -u "$DOMESTIC_DOCKER_USERNAME" -p "$DOMESTIC_DOCKER_PASSWORD" ${DOMESTIC_BASE_NAME}
 		docker push "${newTag}"
 	fi
